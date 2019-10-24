9579037ff5c4800a5a4f913644d03b5331dd0245
liuzhouliang <525688067@qq.com>
2019-10-24 16:06:00 +0800

    flutter

--- dart/lib/方法别名.dart
+++ dart/lib/方法别名.dart
@@ -1,3 +1,7 @@
+/*
+https://juejin.im/post/5d25a6d8f265da1b9163bc7e
+ */
+
 typedef int Compare(Object a, Object b);
 
 class SortedCollection {
@@ -9,6 +13,11 @@ class SortedCollection {
 int sort(_, Object b) {}
 
 main() {
+  /*
+  结论如下：
+方法名（不带括号）作为参数传递给其他方法时，传递的是方法的引用，该方法不会立即执行。
+方法执行语句（带括号）作为参数传递给其他方法时，该方法会立即执行，我们传递过去的是方法的返回值
+   */
   SortedCollection collection = new SortedCollection(sort);
   /*
    // 我们只知道 compare 是一个 Function 类型，
